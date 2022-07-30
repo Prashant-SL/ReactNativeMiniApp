fd3d2d5f19ededd76b3405ccefeb179e62e7ca5f
Prashant-SL <pacchidon07@gmail.com>
2022-07-30 22:34:12 +0530

    Contact page contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,22 +2,39 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 const Contact = () => {
-    const [data, setData] = useState({
-        name: "",
-        age: "",
-        mobile: ""
-    });
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [num, setNum] = useState("");
     const handleSubmit = () => {
-        console.log("Submit done bro");
+        if (name.length <= 5) {
+            Alert.alert("Validation Failed", "Username should be of length more than 5");
+        }
+        else if (age < 18) {
+            Alert.alert("Age Limit", "You are minor to enrol for the course");
+        }
+        else if (num.length != 10) {
+            Alert.alert("Invalid Number", "The number should be of length 10");
+        }
+        else {
+            Alert.alert("Successful", "You will be contacted shortly!!!");
+            navigation.navigate("Home");
+        }
     }
     return (
         <View>
-            <Text>Enter Your Name</Text>
-            <TextInput name="name" style={styles.inpt} />
-            <Text>Enter Your Age</Text>
-            <TextInput name="age" style={styles.inpt} />
-            <Text>Enter Your Mobile Number</Text>
-            <TextInput name="mobile" style={styles.inpt} />
+            <Text style={styles.heading}>Contact Form</Text>
+            <Text style={styles.label}>Enter Your Name</Text>
+            <TouchableOpacity>
+                <TextInput name="name" style={styles.inpt} onChangeText={(e) => setName(e)} />
+            </TouchableOpacity>
+            <Text style={styles.label}>Enter Your Age</Text>
+            <TouchableOpacity>
+                <TextInput name="age" style={styles.inpt} onChangeText={(e) => setAge(e)} />
+            </TouchableOpacity>
+            <Text style={styles.label}>Enter Your Mobile Number</Text>
+            <TouchableOpacity>
+                <TextInput name="mobile" style={styles.inpt} onChangeText={(e) => setNum(e)} />
+            </TouchableOpacity>
 
             <TouchableOpacity onPress={() => handleSubmit()}>
                 <Text>Submit</Text>
@@ -26,8 +43,38 @@ const Contact = () => {
     )
 }
 
+/*
+
+        <View style={{ marginTop: 40 }}>
+            <Text>Contact us regarding any query to us via contact@prashant-sl.netlify.app</Text>
+            <View style={styles.inputContainer}>
+                <Text>Enter Your Email</Text>
+                <TextInput style={styles.input} value={username} onChangeText={(data) => setUsername(data)} />
+            </View>
+            <View style={styles.inputContainer}>
+                <Text>Enter Your Password</Text>
+                <TextInput autoCorrect={false} secureTextEntry={true} style={[styles.input, styles.password]} value={password} onChangeText={(data) => setPassword(data)} />
+            </View>
+            <TouchableOpacity style={styles.btn} onPress={() => submitData()}>
+                <Text style={{ color: "white", padding: 5, textAlign: "center" }}>Submit</Text>
+            </TouchableOpacity>
+        </View>
+
+const FormData = ({ navigation }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const submitData = () => {
+        
+    }
+    return (
+        
+*/
+
 const styles = StyleSheet.create({
-    inpt: { borderWidth: .5, padding: 3 }
+    inpt: { borderWidth: .5, padding: 3 },
+    label: {
+        color: "#55112299"
+    }
 });
 
 export default Contact;
